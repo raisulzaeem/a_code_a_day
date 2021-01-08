0c0fe3aafd36e63b00cdd7e634434a3a2da84331
Raisul Islam Zaeem <raisulzaeem@gmail.com>
2021-01-09 00:00:27 +0100

    fix typo and print result

--- January/day_07.py
+++ January/day_07.py
@@ -1,7 +1,7 @@
 def invalid_parentheses(my_str):
     while True:
         if "()" in my_str:
-            my_str = my_str.replacce("()","")
+            my_str = my_str.replace("()","")
         else:
             break
 
@@ -14,3 +14,5 @@ if __name__ == "__main__":
     input_string = "()()()()()())((()())())()()"
 
     result = invalid_parentheses(input_string)
+
+    print("Number of parentheses to be removed: ", result)
